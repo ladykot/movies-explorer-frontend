d0b0b9fc763a3cbb5d3222651a68b17935a918b7
Katya Akilina <katyaartline@gmail.com>
2023-01-17 18:09:06 +0300

    fix: @media for Header

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,6 +4,7 @@ import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
 import AboutMe from '../AboutMe/AboutMe';
+import './Main.css';
 
 function Main() {
   return (
